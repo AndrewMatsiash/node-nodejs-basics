f2f29a213fec9a2e5408923ee1acd997ef64486a
Андрей <andrej@MacBook-Air.local>
2022-12-05 22:21:18 +0300

    refactor:refactor code

--- src/fs/create.js
+++ src/fs/create.js
@@ -9,10 +9,11 @@ const pathToFile = path.join(__dirname, 'files', 'fresh.txt');
 const create = async () => {
 	try {
 		await fs.access(pathToFile, fs.constants.F_OK);
-		console.error('Error FS operation failed');
 	} catch {
 		fs.writeFile(pathToFile, 'I am fresh and young');
+		return;
 	}
+	throw new Error('Error FS operation failed');
 };
 
 await create();
