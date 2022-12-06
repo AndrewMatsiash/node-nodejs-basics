33378d0cfbaf1426ee974596e2b4354ff3555f63
Андрей <andrej@MacBook-Air.local>
2022-12-06 16:43:59 +0300

    feat:complete tasks zip module

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,6 +1,15 @@
+import { argv } from 'process';
+
+const PREFIX = '--';
+
 const parseArgs = () => {
-	const commandLineArguments = process.argv.map(arg => `propName is ${arg}`).join(', ');
-	console.log(commandLineArguments);
+	const args = argv.reduce(
+		(acc, arg, index, argvArray) =>
+			arg.startsWith(PREFIX) ? [...acc, `${arg.slice(2)} is ${argvArray[index + 1]}`] : acc,
+		[]
+	);
+
+	console.log(args);
 };
 
 parseArgs();
